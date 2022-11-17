2fb170170281cc5ef4584202302973e77e90e857
NIKOLAY NIKITIN <nikitin.nikolay.v@gmail.com>
2022-11-17 22:56:45 +0300

    Initial Commit

---
+++ Dicer/DicerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DicerApp.swift
+//  Dicer
+//
+//  Created by Николай Никитин on 17.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct DicerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
