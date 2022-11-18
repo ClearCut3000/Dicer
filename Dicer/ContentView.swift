d9640eeb69253f9fbae1d58341df467901d2d434
NIKOLAY NIKITIN <nikitin.nikolay.v@gmail.com>
2022-11-18 21:30:21 +0300

    Main app logic and mail UI

--- Dicer/ContentView.swift
+++ Dicer/ContentView.swift
@@ -8,15 +8,76 @@
 import SwiftUI
 
 struct ContentView: View {
+
+  //MARK: - View Properties
+  var diceTypes = [4, 6, 8, 10, 12, 20, 100]
+
+  @AppStorage("selectedDiceType") var selectedDiceType = 6
+  @AppStorage("numberToRoll") var numberToRoll = 4
+
+  @State private var currentResult = DiceResult(type: 0, number: 0)
+
+  let timer = Timer.publish(every: 0.1,
+                            tolerance: 0.1,
+                            on: .main,
+                            in: .common).autoconnect()
+  @State private var stoppedDice = 0
+
+  let columns: [GridItem] = [
+    .init(.adaptive(minimum: 60))
+  ]
+
+  //MARK: - View Body
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+      NavigationView {
+        Form {
+          Section {
+            Picker("Type of Dice", selection: $selectedDiceType) {
+              ForEach(diceTypes, id: \.self) { type in
+                Text("D\(type)")
+              }
+            }
+            .pickerStyle(.segmented)
+            Stepper("Number of Dice: \(numberToRoll)", value: $numberToRoll, in: 1...20)
+            Button("Roll Them!", action: rollDice)
+          } footer: {
+            LazyVGrid(columns: columns) {
+              ForEach(0..<currentResult.rolls.count, id: \.self) { rollNumber in
+                Text(String(currentResult.rolls[rollNumber]))
+                  .frame(maxWidth: .infinity, maxHeight: .infinity)
+                  .aspectRatio(1, contentMode: .fit)
+                  .foregroundColor(.black)
+                  .background(.white)
+                  .cornerRadius(10)
+                  .shadow(radius: 3)
+                  .font(.title)
+                  .padding(5)
+              }
+            }
+          }
+          .disabled(stoppedDice < currentResult.rolls.count)
         }
-        .padding()
+        .navigationTitle("Dicer")
+        .onReceive(timer) { date in
+          updateDice()
+        }
+      }
     }
+
+  //MARK: - View Methods
+  func rollDice() {
+    currentResult = DiceResult(type: selectedDiceType, number: numberToRoll)
+    stoppedDice = 0
+  }
+
+  func updateDice() {
+    guard stoppedDice < currentResult.rolls.count else { return }
+    for i in stoppedDice..<numberToRoll {
+      if i < 0 { continue }
+      currentResult.rolls[i] = Int.random(in: 1...selectedDiceType)
+    }
+    stoppedDice += 1
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
